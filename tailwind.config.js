e4d104b7775f872b7714c4115631522398362e8c
noorimanr@gmail.com <noorimanr@gmail.com>
2023-12-13 10:49:28 +0800

    add dark mode and feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -9,6 +10,7 @@ module.exports = {
     extend: {
       colors: {
         primary: '#c026d3',
+        secondary: '#6b7280',
         dark: '#020617',
       },
       screens: {
